42723b44e4fc927ed4fa72a57aebe98379b03a95
Ricardo Signes <rjbs@semiotic.systems>
2026-03-17 13:24:47 -0400

    box ci-run: run the CI process on a remote box

---
+++ lib/Dobby/Boxmate/App/Command/cirun.pm
@@ -0,0 +1,91 @@
+package Dobby::Boxmate::App::Command::cirun;
+use Dobby::Boxmate::App -command;
+
+# ABSTRACT: inject CI runner into remote box
+
+use v5.36.0;
+use utf8;
+
+sub command_names {
+  return qw(ci-run cirun);
+}
+
+sub abstract { 'inject CI runner into remote box' }
+
+sub usage_desc {
+  '%c ci-inject %o PLANFILE',
+}
+
+sub opt_spec {
+  return (
+    [ 'taskstream|S',  'expect TASK:: protocol directives in the output' ],
+  );
+}
+
+sub validate_args ($self, $opt, $args) {
+  # ...
+}
+
+sub execute ($self, $opt, $args) {
+  my $ip;
+
+  unless (-e -r "misc/test-runner-on-vm") {
+    die "Can't find the CI program we want to inject!\n";
+  }
+
+  require Path::Tiny;
+  require Process::Status;
+  require JSON::XS;
+
+  my $plan_file = $args->[0];
+  my $json = Path::Tiny::path($plan_file)->slurp;
+  my $plan = JSON::XS->new->decode($json);
+
+  my $boxman   = $self->boxman;
+  my $droplet  = $boxman->_get_droplet_for('ci', "ci-run-$plan->{run_id}")->get;
+
+  $ip = $boxman->_ip_address_for_droplet($droplet);
+
+  my @cmd = (
+    qw(
+      scp
+        -o UserKnownHostsFile=/dev/null
+        -o StrictHostKeyChecking=no
+        -o ControlMaster=no
+    ),
+    'misc/test-runner-on-vm',
+    $plan_file,
+    "root\@$ip:/tmp",
+  );
+
+  system @cmd;
+  Process::Status->assert_ok("scping runner to target");
+
+  my @taskstream_env = $opt->taskstream
+                     ? qw( -o SetEnv=FM_TASKSTREAM=1 -o ControlMaster=No )
+                     : ();
+
+  my @ssh_cmd = (
+    qw(
+      ssh
+        -o UserKnownHostsFile=/dev/null
+        -o StrictHostKeyChecking=no
+        -o SendEnv=FM_*
+    ),
+    @taskstream_env,
+    "root\@$ip",
+    "/tmp/test-runner-on-vm --run-id $plan->{run_id}",
+  );
+
+  my $cb = $opt->taskstream
+    ? Dobby::Boxmate::TaskStream->new_taskstream_cb({ loop => $boxman->dobby->loop })
+    : sub ($line, @) { print $line if defined $line };
+
+  my ($exitcode) = $boxman->_run_process_streaming(\@ssh_cmd, $cb)->get;
+
+  $cb->(undef, $exitcode == 0 ? 1 : 0);
+
+  exit($exitcode >> 8) if $exitcode;
+}
+
+1;

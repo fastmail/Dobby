f7341cc59fe0165f93361af38569743c531691ad
Ricardo Signes <rjbs@semiotic.systems>
2026-03-19 10:51:49 -0400

    box ci-retrieve-artifacts: get the logs of what we did

---
+++ lib/Dobby/Boxmate/App/Command/ciretrieve.pm
@@ -0,0 +1,68 @@
+package Dobby::Boxmate::App::Command::ciretrieve;
+use Dobby::Boxmate::App -command;
+
+# ABSTRACT: retrieve test artifacts from a CI box
+
+use v5.36.0;
+use utf8;
+
+sub command_names {
+  return qw(ci-retrieve-artifacts ci-retrieve ciretrieve);
+}
+
+sub abstract { 'retrieve test artifacts from a CI box' }
+
+sub usage_desc {
+  '%c ci-retrieve-artifacts %o PLANFILE',
+}
+
+sub opt_spec {
+  return (
+    [ 'username=s',   "retrieve from a box in this user's namespace", { default => $ENV{USER} }, ],
+  );
+}
+
+sub validate_args ($self, $opt, $args) {
+  @$args == 1 || $self->usage->die;
+
+  my $plan_file = $args->[0];
+  -r $plan_file || die "Can't read plan file $plan_file!\n";
+
+  $opt->username
+    || $self->usage->die({ pre_text => "Neither --username nor \$USER was supplied.\n\n" });
+}
+
+sub execute ($self, $opt, $args) {
+  require Path::Tiny;
+  require Process::Status;
+  require JSON::XS;
+
+  my $plan_file = $args->[0];
+  my $json = Path::Tiny::path($plan_file)->slurp;
+  my $plan = JSON::XS->new->decode($json);
+
+  my $boxman   = $self->boxman;
+  my $droplet  = $boxman->_get_droplet_for($opt->username, "ci-run-$plan->{run_id}")->get;
+
+  $droplet
+    || die "Can't find the droplet to use! Did you run 'box ci-create'?\n";
+
+  my $ip = $boxman->_ip_address_for_droplet($droplet);
+
+  my @cmd = (
+    qw(
+      scp
+        -o UserKnownHostsFile=/dev/null
+        -o StrictHostKeyChecking=no
+        -o ControlMaster=no
+        -r
+    ),
+    "root\@$ip:/tmp/run-" . $plan->{run_id},
+    "./run-$plan->{run_id}",
+  );
+
+  system @cmd;
+  Process::Status->assert_ok("retrieving artifacts to localhost");
+}
+
+1;
